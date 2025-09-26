11d4cdc009b2eb5ec65976ef309c01bbe02bf0c9
Vinayakjaunjal <157274693+Vinayakjaunjal@users.noreply.github.com>
2025-09-26 13:26:12 +0530

    Comment out Twilio settings

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,4 +1,4 @@
-from twilio.rest import Client
+# from twilio.rest import Client #
 from django.conf import settings
 from django.contrib import admin
 from .models import Appointment
@@ -6,19 +6,21 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.http import HttpResponseRedirect
 
-def format_phone(number):
-    # Agar number + se start nahi hota hai to +91 add kar do
-    if not number.startswith("+"):
-        return "+91" + number
-    return number
 
-def send_sms(to_number, message):
-    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-    client.messages.create(
-        body=message,
-        from_=settings.TWILIO_PHONE_NUMBER,
-        to=to_number
-    )
+#def format_phone(number):
+    # Agar number + se start nahi hota hai to +91 add kar do
+   # if not number.startswith("+"):
+     #   return "+91" + number
+    #return number
+
+#def send_sms(to_number, message):
+   # client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+    #client.messages.create(
+      #  body=message,
+       # from_=settings.TWILIO_PHONE_NUMBER,
+       # to=to_number
+   # )
+
 
 from django.contrib import admin
 from .models import Appointment, Service
@@ -43,15 +45,15 @@ class AppointmentAdmin(admin.ModelAdmin):
                 email1.content_subtype = "html"   # 👈 HTML mail
                 email1.send()
                 
-                customer_sms = f"Hello {obj.name}, your appointment on {obj.date} at {obj.time} has been Confirmed."
-                send_sms(format_phone(obj.phone), customer_sms)
+                #customer_sms = f"Hello {obj.name}, your appointment on {obj.date} at {obj.time} has been Confirmed."
+                #send_sms(format_phone(obj.phone), customer_sms)
 
                 email2 = EmailMessage(subject, admin_html, "yourmail@example.com", ["admin@example.com"])
                 email2.content_subtype = "html"   # 👈 HTML mail
                 email2.send()
                 
-                admin_sms = f"Appointment for {obj.name} on {obj.date} at {obj.time} has been Confirmed." 
-                send_sms(settings.ADMIN_MOBILE, admin_sms)
+                #admin_sms = f"Appointment for {obj.name} on {obj.date} at {obj.time} has been Confirmed." 
+                #send_sms(settings.ADMIN_MOBILE, admin_sms)
 
             elif obj.status == "Rejected":
                 subject = "Appointment Rejected ❌"
@@ -62,15 +64,15 @@ class AppointmentAdmin(admin.ModelAdmin):
                 email1.content_subtype = "html"   # 👈 HTML mail
                 email1.send()
                 
-                customer_sms = f"Hello {obj.name}, your appointment on {obj.date} at {obj.time} has been Rejected."
-                send_sms(format_phone(obj.phone), customer_sms)
+                #customer_sms = f"Hello {obj.name}, your appointment on {obj.date} at {obj.time} has been Rejected."
+                #send_sms(format_phone(obj.phone), customer_sms)
 
                 email2 = EmailMessage(subject, admin_html, "yourmail@example.com", ["admin@example.com"])
                 email2.content_subtype = "html"   # 👈 HTML mail
                 email2.send()
                 
-                admin_sms = f"Appointment for {obj.name} on {obj.date} at {obj.time} has been Rejected."
-                send_sms(settings.ADMIN_MOBILE, admin_sms)
+                #admin_sms = f"Appointment for {obj.name} on {obj.date} at {obj.time} has been Rejected."
+                #send_sms(settings.ADMIN_MOBILE, admin_sms)
 
         # Redirect back to admin page after save
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
