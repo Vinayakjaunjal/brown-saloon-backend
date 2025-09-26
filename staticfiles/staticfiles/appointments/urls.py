a617adbb69c4e081aa3f345ea749c6911e8053dc
Vinayakjaunjal <157274693+Vinayakjaunjal@users.noreply.github.com>
2025-09-26 12:05:21 +0530

    Final ready to deploy

--- staticfiles/staticfiles/appointments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('book/', views.book_appointment, name="book_appointment"),
-    
-
-]
